0f2f69560e8aa6959051cc848e2ab0deff004780
Samantha Wittke <32324155+samumantha@users.noreply.github.com>
2024-06-24 17:31:25 +0300

    add repo name to get edit on github button working

--- content/conf.py
+++ content/conf.py
@@ -21,7 +21,7 @@ project = "Snakemake Hackathon"
 copyright = "2024"
 author = "The contributors"
 github_user = "coderefinery"
-github_repo_name = ""  # auto-detected from dirname if blank
+github_repo_name = "snakemake_hackathon"  # auto-detected from dirname if blank
 github_version = "main"
 conf_py_path = "/content/"  # with leading and trailing slash
 
